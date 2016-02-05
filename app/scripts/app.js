1eeab7995c2c9f19a54f77db5f19f20ef74f3952
Temigo <temigo@gmx.com>
2016-02-05 10:53:26 +0100

    Graph display with D3.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.bootstrap'])
+    'ui.bootstrap',
+    'd3'])
     .config(function($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     })
@@ -59,3 +60,29 @@ Just add the following statement during module instantiation:
       var urlBase = 'http://127.0.0.1:8000/graph/infections/';
       return $resource(urlBase, {}, {'query' : {method: 'GET', isArray: false}});
   });
+
+  angular.module('d3', [])
+      .provider('d3Service', function() {
+          function createScript($document, callback, success) {
+              var scriptTag = $document.createElement('script');
+              scriptTag.type = "text/javascript";
+              scriptTag.async = true;
+              scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+              scriptTag.onreadystatechange = function() {
+                  if (this.readyState === 'complete') {
+                      callback();
+                  }
+              };
+              scriptTag.onload = callback;
+              $document.getElementsByTagName('body')[0].appendChild(scriptTag);
+          }
+
+          this.$get = ['$document','$q', '$window', '$rootScope',
+              function($document, $q, $window, $rootScope) {
+                  var deferred = $q.defer();
+                  createScript($document[0], function(callback) {
+                      $rootScope.$apply(function()  { deferred.resolve($window.d3); });
+                  });
+                  return deferred.promise;
+              }];
+      });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,114 +2,129 @@
 
 /**
  * @ngdoc function
- * @name clientApp.controller:MainCtrl
+ * @name whisperApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the clientApp
+ * Controller of the whisperApp
  */
 angular.module('whisperApp')
-    .controller('MainCtrl', function ($scope, Graph, Infection) {
-      $scope.graph_result = Graph.query();
-      $scope.currentIndex = 1;
-      //$scope.currentGraph = $scope.graph_result.results[$scope.currentIndex];
+    .controller('MainCtrl', ['$scope', 'Graph', 'Infection', function ($scope, Graph, Infection) {
+      //$scope.graphResult = Graph.query();
+      Graph.query(function(data) {
+          $scope.graphResult = data.results[0].data;
+          //$scope.graph = $scope.graphResult[0].data;
+      });
 
-      $scope.infection_result = Infection.query();
+      $scope.currentIndex = 1;
+
+      Infection.query(function(data) {
+          $scope.infectionResult = data;
+      });
+
       $scope.currentInfectionIndex = 1;
-    })
-    .directive('graph', function() {
+  }])
+    .directive('d3graph', ['d3Service', function(d3Service) {
         //Constants for the SVG
         var width = 500,
           height = 500;
-        //Set up the colour scale
-        var color = d3.scale.category20();
 
         return {
-          restrict: 'A',
+          restrict: 'EA',
           scope: {
-              val: '=',
-              graph_result: '='
+              data: '=graphData'
           },
           link: function(scope, element, attrs) {
-                scope.$watch('graph_result', function(new_graph_result, old_graph_result) {
-                if (new_graph_result === old_graph_result) {
+              d3Service.then(function(d3) {
+              //Set up the colour scale
+              var color = d3.scale.category20();
+              //Set up the force layout
+              var force = d3.layout.force()
+                  .charge(-120)
+                  .linkDistance(50)
+                  .size([width, height]);
+                  //Append a SVG to the body of the html page. Assign this SVG as an object to svg
+                  var svg = d3.select(element[0]).append("svg")
+                      //.attr("width", width)
+                      .attr("height", height)
+                      .style("width", "100%");
+
+            scope.$watch('data', function(newData, oldData) {
+                svg.selectAll('*').remove();
+                if (!newData) { // || newData === oldData
                     return;
                 }
-                else {
-                    console.log(new_graph_result);
-                    currentGraph = new_graph_result.results[scope.currentIndex];
-                    //Set up the force layout
-                    var force = d3.layout.force()
-                        .charge(-120)
-                        .linkDistance(50)
-                        .size([width, height]);
-                    //Append a SVG to the body of the html page. Assign this SVG as an object to svg
-                    var svg = d3.select("#canvas").append("svg")
-                        .attr("width", width)
-                        .attr("height", height);
+                //else {
+                    // TODO scope.currentIndex
+                    var currentGraph = angular.fromJson(newData);
                     //Read the data from the mis element
-                    //var nodes = {{nodes|safe}};
                     var nodes = currentGraph.nodes;
                     var links = currentGraph.links;
+                    //var nodes = [{'id': 1}, {'id': 2}];
+                    //var links = [];
                     var infected_nodes = [];
+
 
                     for (var d in nodes) {
                     	if (d in infected_nodes) { nodes[d].infected = true; } else {nodes[d].infected = false; }
                     }
-
-
 
                     //Creates the graph data structure out of the json data
                     force.nodes(nodes)
                         .links(links)
                         .start();
 
-                    //Create all the line svgs but without locations yet
-                    var link = svg.selectAll(".link")
-                        .data(links)
-                        .enter().append("line")
-                        .attr("class", "link")
-                        .style("stroke-width", function (d) {
-                        return 1;
-                    });
-
-                    //Do the same with the circles for the nodes - no
-                    var node = svg.selectAll(".node")
-                        .data(nodes)
-                        .enter().append("circle")
-                        .attr("class", "node")
-                        .attr("r", 8)
-                        .style("fill", function (d) {
-                        if (d.infected) { color_index = 0; } else { color_index = 1; }
-                        return color(color_index);
-                    })
-                        .call(force.drag)
-                        .on('dblclick', connectedNodes);
-
-
-                    //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-                    force.on("tick", function () {
-                        link.attr("x1", function (d) {
-                            return d.source.x;
-                        })
-                            .attr("y1", function (d) {
-                            return d.source.y;
-                        })
-                            .attr("x2", function (d) {
-                            return d.target.x;
-                        })
-                            .attr("y2", function (d) {
-                            return d.target.y;
+                        //Create all the line svgs but without locations yet
+                        var link = svg.selectAll(".link")
+                            .data(links)
+                            .enter().append("line")
+                            .attr("class", "link")
+                            .style("stroke-width", function (d) {
+                            return 1;
                         });
 
-                        node.attr("cx", function (d) {
-                            return d.x;
+                        //Do the same with the circles for the nodes - no
+                        var color_index = 1;
+                        var connectedNodes = [];
+                        var node = svg.selectAll(".node")
+                            .data(nodes)
+                            .enter().append("circle")
+                            .attr("class", "node")
+                            .attr("r", 8)
+                            .style("fill", function (d) {
+                            if (d.infected) { color_index = 0; } else { color_index = 1; }
+                            return color(color_index);
                         })
-                            .attr("cy", function (d) {
-                            return d.y;
-                        });
-                    });
-                }});
+                            .call(force.drag)
+                            .on('dblclick', connectedNodes);
 
-          }
+
+                        //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+                        force.on("tick", function () {
+                            link.attr("x1", function (d) {
+                                return d.source.x;
+                            })
+                                .attr("y1", function (d) {
+                                return d.source.y;
+                            })
+                                .attr("x2", function (d) {
+                                return d.target.x;
+                            })
+                                .attr("y2", function (d) {
+                                return d.target.y;
+                            });
+
+                            node.attr("cx", function (d) {
+                                return d.x;
+                            })
+                                .attr("cy", function (d) {
+                                return d.y;
+                            });
+                        });
+
+                //}
+            });
+        //});//
+          });
+      }
       };
-  });
+  }]);
