2325cc0bf1858c02cf567c893333271906819780
Temigo <temigo@gmx.com>
2016-02-29 19:23:31 +0100

    Upload is working

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# client
-
-This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
-version 0.15.1.
+# whisper-app-client
+Client for Whisper App
 
 ## Build & development
 
-Run `grunt` for building and `grunt serve` for preview.
+Run `grunt build` for building and `grunt serve` for preview.
 
 ## Testing
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,41 +9,8 @@
  */
 angular.module('whisperApp')
     .controller('MainCtrl', ['$scope', '$window', 'Graph', 'Infection', 'Algorithm', 'GenerateGraph', '$timeout', 'FileSaver', 'Blob', function ($scope, $window, Graph, Infection, Algorithm, GenerateGraph, $timeout, FileSaver, Blob) {
-        /*$scope.uploader = new FileUploader({method: 'GET'});
-        console.log($scope.uploader);
-        $scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-            var graph = angular.fromJson(fileItem.url);
-            console.log("Hi");
-        };
-        $scope.upload = {};
-        $scope.upload.src = "";
-        $scope.run_upload = function(file) {
-                console.log(file);
-        };*/
-            $scope.uploadFiles = function(file, errFiles) {
-                $scope.f = file;
-                $scope.errFile = errFiles && errFiles[0];
-                if (file) {
-                    console.log(file);
-                    angular.fromJson(file);
-                    /*file.upload = Upload.upload({
-                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                        data: {file: file}
-                    });
 
-                    file.upload.then(function (response) {
-                        $timeout(function () {
-                            file.result = response.data;
-                        });
-                    }, function (response) {
-                        if (response.status > 0)
-                            $scope.errorMsg = response.status + ': ' + response.data;
-                    }, function (evt) {
-                        file.progress = Math.min(100, parseInt(100.0 *
-                                                 evt.loaded / evt.total));
-                    });*/
-                }
-            };
+
 
       //$scope.graphResult = Graph.query();
       $scope.currentIndex = 0;
@@ -144,134 +111,13 @@ angular.module('whisperApp')
         FileSaver.saveAs(new Blob([pretty_data], {type: "application/json"}), "graph.json");
     };
 
-  }])
-    .directive('d3graph', ['d3Service', function(d3Service) {
-        //Constants for the SVG
-        var width = 500,
-          height = 500;
+    $scope.uploadInfection = function($fileContent){
+        $scope.currentInfection = $fileContent;
+    };
+    $scope.uploadGraph = function($fileContent){
+        $scope.currentGraph = $fileContent;
+    };
 
-        return {
-          restrict: 'EA',
-          scope: {
-              data: '@graphData', // One-way data binding
-              infectionData: '@infectionData',
-              infectMode: '=',
-              source: '=',
-              infectNode: '&'
-          },
-          link: function(scope, element, attrs) {
-              d3Service.then(function(d3) {
-              //Set up the colour scale
-              var color = d3.scale.category20().domain(d3.range(0,20));
-
-              //Set up the force layout
-              var force = d3.layout.force()
-                  .charge(-120)
-                  .linkDistance(50)
-                  .size([width, height]);
-              //Append a SVG to the body of the html page. Assign this SVG as an object to svg
-              var svg = d3.select(element[0]).append("svg")
-                  //.attr("width", width)
-                  .attr("height", height)
-                  .call(d3.behavior.zoom().on("zoom", function () {
-                      svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-                    }))
-                  .style("width", "100%");
-
-            //console.log(scope.infectionData);
-            scope.$watchGroup(['data', 'infectionData', 'source'], function(newData, oldData) {
-                svg.selectAll('*').remove();
-                if (!newData) { // || newData === oldData
-                    return;
-                }
-                    var currentGraph = angular.fromJson(newData[0]);
-
-                    //Read the data from the mis element
-                    var nodes = currentGraph.nodes;
-                    var links = currentGraph.links;
-                    var infected_nodes = angular.fromJson(newData[1]).nodes;
-
-                    for (var d in nodes) {
-                        nodes[d].selected = false;
-                    	if (d in infected_nodes) { nodes[d].infected = true; } else {nodes[d].infected = false; }
-                        if (d == scope.source) { nodes[d].source = true; } else {nodes[d].source = false; }
-                    }
-                    //Creates the graph data structure out of the json data
-                    force.nodes(nodes)
-                        .links(links)
-                        .start();
-
-                        //Create all the line svgs but without locations yet
-                        var link = svg.selectAll(".link")
-                            .data(links)
-                            .enter().append("line")
-                            .attr("class", "link")
-                            .style("stroke-width", function (d) {
-                            return 1;
-                        });
-
-
-                        var standardColor = function(d) {
-                            var color_index = 1;
-                            if (d.infected) { color_index = 0; }
-                            if (d.source) { color_index = 2; }
-                            if (d.selected) { color_index = 5; }
-                            return color(color_index);
-                        };
-
-                        var highlightNode = function(d) {
-                            var node = d3.select(this);
-                            if (!scope.infectMode) {
-                                d.selected = !d.selected;
-                            }
-                            else { // infectMode
-                                scope.infectNode({node: d, infected: d.infected});
-                                d.infected = !d.infected;
-                            }
-                            node.style("fill", standardColor);
-                        };
-
-                        //Do the same with the circles for the nodes - no
-                        //var color_index = 1;
-                        var connectedNodes = [];
-                        var node = svg.selectAll(".node")
-                            .data(nodes)
-                            .enter().append("circle")
-                            .attr("class", "node")
-                            .attr("r", 8)
-                            .style("fill", standardColor)
-                            //.style("fill", color(5))
-                            .call(force.drag)
-                            .on('click', highlightNode);
-
-
-                        //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-                        force.on("tick", function () {
-                            link.attr("x1", function (d) {
-                                return d.source.x;
-                            })
-                                .attr("y1", function (d) {
-                                return d.source.y;
-                            })
-                                .attr("x2", function (d) {
-                                return d.target.x;
-                            })
-                                .attr("y2", function (d) {
-                                return d.target.y;
-                            });
-
-                            node.attr("cx", function (d) {
-                                return d.x;
-                            })
-                                .attr("cy", function (d) {
-                                return d.y;
-                            });
-                        });
-                        // End force tick
-                    }); // scope watch
-                }); // d3Service.then
-            } // link
-        }; // return
   }])
   .controller('existingGraphCtrl', ['$scope', function($scope) {
       $scope.watch('currentIndex', function(newVal, oldVal) {
@@ -281,4 +127,25 @@ angular.module('whisperApp')
             //}
          }
      });
-  }]);
+  }])
+  .directive('onReadFile', function ($parse) {
+ 	return {
+ 		restrict: 'A',
+ 		scope: false,
+ 		link: function(scope, element, attrs) {
+             var fn = $parse(attrs.onReadFile);
+
+ 			element.on('change', function(onChangeEvent) {
+ 				var reader = new FileReader();
+
+ 				reader.onload = function(onLoadEvent) {
+ 					scope.$apply(function() {
+ 						fn(scope, {$fileContent:onLoadEvent.target.result});
+ 					});
+ 				};
+
+ 				reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+ 			});
+ 		}
+ 	};
+});
